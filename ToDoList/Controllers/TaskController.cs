b74adeaf490c89b5da31fa63c34c3123d7aa0599
Roman <Kostya59ru93@gamil.com>
2023-07-05 17:54:04 +0300

    Database connected

---
+++ ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
